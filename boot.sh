e21f002843a04265daf15fd756c6b4f03cd9dd21
B. Bueno <bbueno5000@gmail.com>
2019-04-16 21:46:05 -0600

    edited shell files

--- boot.sh
+++ boot.sh
@@ -1,17 +1,22 @@
-#Stop the background process
+# stop the background process
 sudo /etc/init.d/bluetooth stop
-# Turn on Bluetooth
-sudo hciconfig hcio up
-# Update  mac address
-./updateMac.sh
-#Update Name
-./updateName.sh ThanhLe_Keyboard
-#Get current Path
-export C_PATH=$(pwd)
-#Create Tmux session
-tmux has-session -t  mlabviet
-if [ $? != 0 ]; then
 
+# turn on Bluetooth
+sudo hciconfig hcio up
+
+# update mac address
+./updateMac.sh
+
+# update name
+./updateName.sh arcade-controller
+
+# get current path
+export C_PATH=$(pwd)
+
+# create Tmux session
+tmux has-session -t  mlabviet
+
+if [ $? != 0 ]; then
     tmux new-session -s mlabviet -n os -d
     tmux split-window -h -t mlabviet
     tmux split-window -v -t mlabviet:os.0
---
+++ update_name.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+a="\"$1\""
+
+sed -i -e "s/\(MY_DEV_NAME=\).*/\1$a/" server/btk_server.py
